85e89c3d03c7f365c8f79d017005f0c81047f8ae
hankkyy <hankzhang@Hanks-MacBook-Pro.local>
2026-06-22 17:03:07 +0800

    fix: add missing useMemo import

--- src/pages/market-watch/[id].tsx
+++ src/pages/market-watch/[id].tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useEffect, useState, useCallback, useRef } from "react";
+import { useEffect, useMemo, useState, useCallback, useRef } from "react";
 import {
   Anchor,
   Badge,
